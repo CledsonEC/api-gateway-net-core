c53bcf6f0d0c3a41322147dae47cc88d0685c2dc
Cledson <cledson.caetano@gmail.com>
2019-11-22 15:37:17 -0300

    Exemplo de Api Gateway com ocelot

---
+++ ClientesApi/Controllers/ClientesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET api/clientes
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var c1 = new Cliente(1, "Cliente ABC");
+            var c2 = new Cliente(2, "Cliente XYZ");
+            return Ok(new List<Cliente>{c1,c2});
+        }
+    }
+
+    public class Cliente
+    {
+        public int Id {get; set;}
+        public string Nome {get ;set;}
+
+        public Cliente(int id, string nome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+        }
+    }
+}
---
+++ ProdutosApi/Controllers/ProdutosController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        // GET api/produtos
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var p1 = new Produto(1, "Produto 123");
+            var p2 = new Produto(2, "Produto 456");
+            return Ok(new List<Produto> { p1, p2 });
+        }
+
+        // GET api/produtos/Categorias
+        [HttpGet("categorias")]
+        public IActionResult GetCategorias()
+        {
+            return Ok(new List<string> { "Categoria A", "Categoria B" });
+        }
+    }
+
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        public Produto(int id, string descricao)
+        {
+            this.Id = id;
+            this.Descricao = descricao;
+        }
+    }
+}
